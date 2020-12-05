623adee4bec4622cdce81c6b06932c38398362e9
Владимир <gaiduk_vladimir@mail.ru>
2020-12-05 08:13:18 +0300

    Add selenide

---
+++ src/test/java/SberCreditCardTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class SberCreditCardTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+}
---
+++ src/test/java/test/base/SettingsTests.java
@@ -0,0 +1,17 @@
+package test.base;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class SettingsTests {
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+}
